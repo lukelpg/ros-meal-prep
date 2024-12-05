c7c34a9af775cb2a7f562865d7d75b299ff8c06d
lukelpg <20lapg1@queensu.ca>
2024-12-04 20:28:27 -0500

    Adding serial communication test

---
+++ test/joyStickC/joyStickC.ino
@@ -0,0 +1,21 @@
+#define joyX A0
+#define joyY A1
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  float xValue = analogRead(joyX);
+  float yValue = analogRead(joyY);
+ 
+  //print the values with to plot or view
+  Serial.print(xValue);
+  Serial.print("\t");
+  Serial.println(yValue);
+
+  Serial.println("Hello from Arduino!");
+  delay(1000);
+
+}
