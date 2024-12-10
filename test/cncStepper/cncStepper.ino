83117e0a9ee975354b340208fe69cfd6f0a581b1
lukelpg <20lapg1@queensu.ca>
2024-12-09 23:40:10 -0500

    Started to add Z axis motor

--- test/cncStepper/cncStepper.ino
+++ test/cncStepper/cncStepper.ino
@@ -4,10 +4,16 @@
 #define MOTOR_Y_ENABLE_PIN 8
 #define MOTOR_Y_STEP_PIN 3
 #define MOTOR_Y_DIR_PIN 6
-#define LIMIT_SWITCH_PIN 10  // Y- pin is typically 14 on the CNC shield (check your board's documentation)
+#define LIMIT_SWITCH_PIN_Y 10
 
-// Initialize the motor
-AccelStepper motorY(1, MOTOR_Y_STEP_PIN, MOTOR_Y_DIR_PIN);
+#define MOTOR_Z_ENABLE_PIN 8   // Enable pin for Motor Z
+#define MOTOR_Z_STEP_PIN 4     // Step pin for Motor Z
+#define MOTOR_Z_DIR_PIN 7      // Direction pin for Motor Z
+#define LIMIT_SWITCH_PIN_Z 11
+
+// Initialize the motors
+AccelStepper motorY(1, MOTOR_Y_STEP_PIN, MOTOR_Y_DIR_PIN);  // Motor Y
+AccelStepper motorZ(1, MOTOR_Z_STEP_PIN, MOTOR_Z_DIR_PIN);  // Motor Z
 
 int motorDirection = 0;
 bool home = false;
@@ -18,27 +24,46 @@ void setup()
    
    // Set motor enable and direction pins as output
    pinMode(MOTOR_Y_ENABLE_PIN, OUTPUT);
-   pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);  // Y- pin is configured as input with pull-up resistor
+   pinMode(LIMIT_SWITCH_PIN_Y, INPUT_PULLUP);  // Y- pin is configured as input with pull-up resistor
+
+   pinMode(MOTOR_Z_ENABLE_PIN, OUTPUT);
+   pinMode(LIMIT_SWITCH_PIN_Z, INPUT_PULLUP);
+   
 
    motorY.setEnablePin(MOTOR_Y_ENABLE_PIN);
    motorY.setPinsInverted(false, false, true);
    motorY.setAcceleration(1000);
    motorY.setMaxSpeed(750); // Set max speed for the motor
    motorY.enableOutputs();
+
+   motorZ.setEnablePin(MOTOR_Y_ENABLE_PIN);
+   motorZ.setPinsInverted(false, false, true);
+   motorZ.setAcceleration(1000);
+   motorZ.setMaxSpeed(750); // Set max speed for the motor
+   motorZ.enableOutputs();
    
    // Explicitly set the direction of the motor on startup (choose a direction)
    digitalWrite(MOTOR_Y_DIR_PIN, LOW);  // Set the direction to LOW (change to HIGH if necessary)
+   digitalWrite(MOTOR_Z_DIR_PIN, LOW);
 }
 
 
 void homeMotor() {
-  while (digitalRead(LIMIT_SWITCH_PIN) == 0) {
+  while (digitalRead(LIMIT_SWITCH_PIN_Y) == 0) {
     motorY.moveTo(20000);
     motorY.run();
   }
 
   motorY.setCurrentPosition(0);             
   motorY.stop();
+
+  while (digitalRead(LIMIT_SWITCH_PIN_Z) == 0) {
+    motorZ.moveTo(20000);
+    motorY.run();
+  }
+  
+  motorZ.setCurrentPosition(0);             
+  motorZ.stop();
 }
 
 
