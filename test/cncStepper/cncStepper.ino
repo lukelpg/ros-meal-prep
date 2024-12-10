27ed595d5251b80b0c2b69ab858da52ccb18ab0e
lukelpg <20lapg1@queensu.ca>
2024-12-10 11:35:36 -0500

    Second driver confirmed to be working

--- test/cncStepper/cncStepper.ino
+++ test/cncStepper/cncStepper.ino
@@ -1,12 +1,12 @@
 #include <AccelStepper.h>
 
 // Pin Definitions
-#define MOTOR_Y_ENABLE_PIN 8
+#define ENABLE_PIN 8
+
 #define MOTOR_Y_STEP_PIN 3
 #define MOTOR_Y_DIR_PIN 6
 #define LIMIT_SWITCH_PIN_Y 10
 
-#define MOTOR_Z_ENABLE_PIN 8   // Enable pin for Motor Z
 #define MOTOR_Z_STEP_PIN 4     // Step pin for Motor Z
 #define MOTOR_Z_DIR_PIN 7      // Direction pin for Motor Z
 #define LIMIT_SWITCH_PIN_Z 11
@@ -20,26 +20,24 @@ bool home = false;
 
 void setup()
 {
-   Serial.begin(9600);  
+   // Serial.begin(9600);  
    
    // Set motor enable and direction pins as output
-   pinMode(MOTOR_Y_ENABLE_PIN, OUTPUT);
+   pinMode(ENABLE_PIN, OUTPUT);
    pinMode(LIMIT_SWITCH_PIN_Y, INPUT_PULLUP);  // Y- pin is configured as input with pull-up resistor
-
-   pinMode(MOTOR_Z_ENABLE_PIN, OUTPUT);
    pinMode(LIMIT_SWITCH_PIN_Z, INPUT_PULLUP);
    
 
-   motorY.setEnablePin(MOTOR_Y_ENABLE_PIN);
+   motorY.setEnablePin(ENABLE_PIN);
+   
    motorY.setPinsInverted(false, false, true);
    motorY.setAcceleration(1000);
    motorY.setMaxSpeed(750); // Set max speed for the motor
    motorY.enableOutputs();
 
-   motorZ.setEnablePin(MOTOR_Y_ENABLE_PIN);
    motorZ.setPinsInverted(false, false, true);
    motorZ.setAcceleration(1000);
-   motorZ.setMaxSpeed(750); // Set max speed for the motor
+   motorZ.setMaxSpeed(500); // Set max speed for the motor
    motorZ.enableOutputs();
    
    // Explicitly set the direction of the motor on startup (choose a direction)
@@ -49,17 +47,17 @@ void setup()
 
 
 void homeMotor() {
-  while (digitalRead(LIMIT_SWITCH_PIN_Y) == 0) {
-    motorY.moveTo(20000);
-    motorY.run();
-  }
-
-  motorY.setCurrentPosition(0);             
-  motorY.stop();
+//  while (digitalRead(LIMIT_SWITCH_PIN_Y) == 0) {
+//    motorY.moveTo(20000);
+//    motorY.run();
+//  }
+//
+//  motorY.setCurrentPosition(0);             
+//  motorY.stop();
 
   while (digitalRead(LIMIT_SWITCH_PIN_Z) == 0) {
     motorZ.moveTo(20000);
-    motorY.run();
+    motorZ.run();
   }
   
   motorZ.setCurrentPosition(0);             
@@ -73,8 +71,11 @@ void loop()
     homeMotor();
     home = true;
   }else{
-    motorY.moveTo(-6000);
-    motorY.run();
+    motorZ.moveTo(-3000);
+    motorZ.run();
+
+//    motorY.moveTo(-6000);
+//    motorY.run();
   }
 
 }
